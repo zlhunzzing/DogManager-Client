f114567a9130bd9e0c25591602e70c41bbaa7267
J-E-Y <rightfuljung@gmail.com>
2020-03-26 16:11:30 +0900

    built structure of adminCouponVeiwPage

---
+++ src/containers/AdminCouponViewContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const AdminCouponViewContainer: React.FunctionComponent = () => {
+  return (
+    <div>
+      <div>here is AdminCouponViewContainer</div>
+    </div>
+  );
+};
+
+export default AdminCouponViewContainer;
