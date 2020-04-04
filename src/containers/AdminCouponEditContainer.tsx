c5ba16793caa9e2b37d33471229ccbf765e52a38
J-E-Y <rightfuljung@gmail.com>
2020-04-04 17:09:25 +0900

    AdminCouponEdit UI

--- src/containers/AdminCouponEditContainer.tsx
+++ src/containers/AdminCouponEditContainer.tsx
@@ -151,8 +151,11 @@ const AdminCouponEditContainer: React.FunctionComponent<CouponEditContainerProps
         onSubmit={handleSubmitCouponEdit}
       >
         <div className={classes.root}>
-          <div style={{ paddingRight: 50, paddingTop: 20, margin: 10 }}>
-            <span style={{ fontWeight: 'bold', paddingRight: 20 }}> 쿠폰이름</span>
+          <div style={{ paddingRight: 50, paddingTop: 20, margin: 10, marginTop: -34 }}>
+            <span style={{ fontWeight: 'bold', paddingRight: 20, marginLeft: -20 }}>
+              {' '}
+              쿠폰이름
+            </span>
             <TextField
               id="standard-textarea"
               style={{ paddingRight: 50 }}
@@ -191,10 +194,10 @@ const AdminCouponEditContainer: React.FunctionComponent<CouponEditContainerProps
             />
           </div>
           <div style={{ paddingRight: 50, paddingTop: 20, margin: 10 }}>
-            <span style={{ fontWeight: 'bold' }}>쿠폰 유효기간</span>
+            <span style={{ fontWeight: 'bold', marginLeft: -31 }}>쿠폰 유효기간</span>
             <TextField
               id="standard-textarea"
-              placeholder="숫자만 적어주세요 ex)7동안 => 7"
+              placeholder="숫자만 적어주세요 ex) 7동안 => 7"
               style={{ paddingRight: 65 }}
               multiline
               onChange={(event): void => {
@@ -217,7 +220,11 @@ const AdminCouponEditContainer: React.FunctionComponent<CouponEditContainerProps
             />
           </div>
         </div>
-        <Button style={{ width: 100, marginRight: 10 }} variant="outlined" type="submit">
+        <Button
+          style={{ width: 100, marginRight: 10, marginTop: -44 }}
+          variant="outlined"
+          type="submit"
+        >
           등록
         </Button>
       </form>
